46de46d5d1282c2628922208d356750324fe85d0
Afathi_IT24100052 <IT24100052@my.sliit.lk>
2025-05-19 21:19:13 +0530

    Update UserDeleteServlet.java

--- src/main/java/com/example/demo/controllers/user/UserDeleteServlet.java
+++ src/main/java/com/example/demo/controllers/user/UserDeleteServlet.java
@@ -9,20 +9,19 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
 
 
-@WebServlet(name = "UserDeleteServlet", value = "/delete-user")
+@WebServlet(name = "UserDeleteServlet", value = "/delete-user") // this servlet delete a user when called at /delete-user
 public class UserDeleteServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) // this method runs when a POST request is made to /delete-user
             throws ServletException, IOException {
         UserManager.readUsers();
 
-        int id = Integer.parseInt(request.getParameter("user-id"));
+        int id = Integer.parseInt(request.getParameter("user-id"));  // get the user ID from the request
 
-        UserManager.removeUser(id);
+        UserManager.removeUser(id);    // delete user with given ID
 
-        response.sendRedirect("pages/user/admin-view.jsp");
+        response.sendRedirect("pages/user/admin-view.jsp"); // After deleting go back to admin user management page
     }
 }
